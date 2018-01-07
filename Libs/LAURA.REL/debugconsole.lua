d83915c23509575d28cdfecdd0cb4acccc17f7cc
Tricky1975 <facebook@tbbs.nl>
2018-01-07 13:53:25 +0100

    License

--- Libs/LAURA.REL/debugconsole.lua
+++ Libs/LAURA.REL/debugconsole.lua
@@ -1,3 +1,13 @@
+--[[
+        debugconsole.lua
+	(c) 2018 Jeroen Petrus Broks.
+	
+	This Source Code Form is subject to the terms of the 
+	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
+	distributed with this file, You can obtain one at 
+	http://mozilla.org/MPL/2.0/.
+        Version: 18.01.07
+]]
 
 
 -- $USE libs/console
